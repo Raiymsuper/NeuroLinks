44ee3566267ea9aa1c3b887d2bb02af9e8d8ec61
Raiymbek K <raiymbek200516@gmail.com>
2025-02-23 01:52:31 +0500

    Lab 2

--- LAB 1/main.py
+++ LAB 1/main.py
@@ -1,6 +1,5 @@
 import numpy as np
 # import matplotlib.pyplot as plt
-
 def act(X):
     return 0 if X <= 0 else 1
 
@@ -10,7 +9,6 @@ def go(C):
     w2 = [1, 1, -0.5]
     w_hidden = np.array([w1, w2])
     w_out = np.array([-1, 1, -0.5])
-
     sum = np.dot(w_hidden, x)
     out = [act(x) for x in sum]
     out.append(1)
@@ -24,4 +22,3 @@ C2 = [(0, 0),(1, 1)]
 
 print(go(C1[0]), go(C1[1]))
 print(go(C2[0]), go(C2[1]))
-
